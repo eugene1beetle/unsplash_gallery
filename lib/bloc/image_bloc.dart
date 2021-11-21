90b2e6d01530768fc9b13a545f2bf875d81abf1c
Eugene Beetle <eugene.beetle.01@gmail.com>
2021-11-21 21:09:41 +0200

    create current page field

--- lib/bloc/image_bloc.dart
+++ lib/bloc/image_bloc.dart
@@ -2,8 +2,8 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:unsplash_gallery/api/unsplash_api.dart';
 
+import 'package:unsplash_gallery/api/unsplash_api.dart';
 import 'package:unsplash_gallery/model/unsplash_image.dart';
 
 part 'image_event.dart';
@@ -16,12 +16,15 @@ class ImageBloc extends Bloc<ImageEvent, ImageState> {
 
   Future<void> _onFetchImages(
       FetchImages event, Emitter<ImageState> emit) async {
-    emit(state.copyWith(status: Status.fetching));
+    if (state.hasReachedMax) return;
+    emit(state.copyWith(status: Status.fetching, hasReachedMax: true));
     try {
-      final images = await UnsplashApi.fetchImages();
+      final images = await UnsplashApi.fetchImages(page: state.currentPage);
       emit(state.copyWith(
         status: Status.success,
         images: [...state.images, ...images],
+        currentPage: state.currentPage + 1,
+        hasReachedMax: false,
       ));
     } catch (e) {
       emit(state.copyWith(status: Status.failure));
--- lib/bloc/image_state.dart
+++ lib/bloc/image_state.dart
@@ -12,29 +12,33 @@ class ImageState extends Equatable {
     this.status = Status.initial,
     this.images = const <UnsplashImage>[],
     this.hasReachedMax = false,
+    this.currentPage = 1,
   });
 
   final Status status;
   final List<UnsplashImage> images;
   final bool hasReachedMax;
+  final int currentPage;
 
   ImageState copyWith({
     Status? status,
     List<UnsplashImage>? images,
     bool? hasReachedMax,
+    int? currentPage,
   }) {
     return ImageState(
       status: status ?? this.status,
       images: images ?? this.images,
       hasReachedMax: hasReachedMax ?? this.hasReachedMax,
+      currentPage: currentPage ?? this.currentPage,
     );
   }
 
   @override
   String toString() {
-    return 'ImageState{status: $status, images: $images, hasReachedMax: $hasReachedMax}';
+    return 'ImageState{status: $status, images: $images, hasReachedMax: $hasReachedMax, currentPage: $currentPage}';
   }
 
   @override
-  List<Object> get props => [status, images, hasReachedMax];
+  List<Object> get props => [status, images, hasReachedMax, currentPage];
 }
