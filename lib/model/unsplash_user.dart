f69df6ab43ef9b8a2fcbb02052e5a5309a4466b7
Eugene Beetle <eugene.beetle.01@gmail.com>
2021-11-22 19:37:09 +0200

    add twitter and instagram accounts to user model

--- lib/model/unsplash_user.dart
+++ lib/model/unsplash_user.dart
@@ -5,15 +5,18 @@ part 'unsplash_user.g.dart';
 @JsonSerializable()
 class User {
   final String name, username;
+  final String? instagram_username, twitter_username;
 
   const User({
     required this.name,
     required this.username,
+    this.instagram_username,
+    this.twitter_username,
   });
 
   @override
   String toString() {
-    return 'User{name: $name, username: $username}';
+    return 'User{name: $name, username: $username, instagram_username: $instagram_username, twitter_username: $twitter_username}';
   }
 
   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
--- lib/model/unsplash_user.g.dart
+++ lib/model/unsplash_user.g.dart
@@ -9,9 +9,13 @@ part of 'unsplash_user.dart';
 User _$UserFromJson(Map<String, dynamic> json) => User(
       name: json['name'] as String,
       username: json['username'] as String,
+      instagram_username: json['instagram_username'] as String?,
+      twitter_username: json['twitter_username'] as String?,
     );
 
 Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
       'name': instance.name,
       'username': instance.username,
+      'instagram_username': instance.instagram_username,
+      'twitter_username': instance.twitter_username,
     };
